97d3582ad9a42443b641c280a1fa887d4a761e89
hasanshahrear <hasanshahrear143@gmail.com>
2024-12-03 22:40:00 +0600

    update find all in design type & customer

--- src/design-type/design-type.controller.ts
+++ src/design-type/design-type.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Get,
   Param,
+  ParseBoolPipe,
   Patch,
   Post,
   Put,
@@ -28,10 +29,14 @@ export class DesignTypeController {
   }
 
   @Get()
-  async findAll(@Query('page') page: string, @Query('limit') limit: string) {
+  async findAll(
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+    @Query('status', new ParseBoolPipe()) status: boolean,
+  ) {
     const pageNum = parseInt(page, 10) || 1;
     const limitNum = parseInt(limit, 10) || 10;
-    return await this.designTypeService.findAll(pageNum, limitNum);
+    return await this.designTypeService.findAll(pageNum, limitNum, status);
   }
 
   @Get(':id')
