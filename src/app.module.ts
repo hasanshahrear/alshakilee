f25c9660b0ac88f6f5c1537b804028aefd5515bf
hasanshahrear <hasanshahrear143@gmail.com>
2025-05-11 01:48:02 +0600

    working on invoice module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
-import { DesignTypeModule } from './design-type/design-type.module';
-import { LoggerModule } from './logger/logger.module';
-import { BottomTypeModule } from './bottom-type/bottom-type.module';
 import { InvoicesModule } from './invoices/invoices.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [
-    LoggerModule,
-    CustomerModule,
-    DesignTypeModule,
-    BottomTypeModule,
-    InvoicesModule,
-  ],
+  imports: [LoggerModule, CustomerModule, InvoicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/sql-functions.sql
@@ -0,0 +1,30 @@
+-- Create the serial tracker table
+CREATE TABLE IF NOT EXISTS daily_invoice_serials (
+  serial_date DATE PRIMARY KEY,
+  current_serial INT NOT NULL DEFAULT 0
+);
+
+-- Drop the function if it already exists (optional)
+DROP FUNCTION IF EXISTS get_next_invoice_serial;
+
+-- Create the function to generate next invoice serial
+DELIMITER $$
+
+CREATE FUNCTION get_next_invoice_serial(p_date DATE)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+  DECLARE next_serial INT;
+
+  INSERT INTO daily_invoice_serials (serial_date, current_serial)
+  VALUES (p_date, 1)
+  ON DUPLICATE KEY UPDATE current_serial = current_serial + 1;
+
+  SELECT current_serial INTO next_serial
+  FROM daily_invoice_serials
+  WHERE serial_date = p_date;
+
+  RETURN next_serial;
+END$$
+
+DELIMITER ;
