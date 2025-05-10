3133df923dbc9ca6694f80140e31140810c00ef5
hasanshahrear <hasanshahrear143@gmail.com>
2025-05-11 01:48:02 +0600

    working on invoice module

--- src/invoices/invoices.service.ts
+++ src/invoices/invoices.service.ts
@@ -1,4 +1,5 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { isValid, parseISO } from 'date-fns';
 import { HttpResponseService } from 'src/http-response/http-response.service';
 import { LoggerService } from 'src/logger/logger.service';
@@ -17,12 +18,22 @@ export class InvoicesService {
   ) {}
 
   async create(createInvoiceDto: CreateInvoiceDto) {
+    const today = new Date().toISOString().split('T')[0];
+    const [{ next_serial }] = await this._prisma.$queryRawUnsafe<
+      { next_serial: number }[]
+    >(`SELECT get_next_invoice_serial('${today}') AS next_serial;`);
+
+    const yy = today.slice(2, 4);
+    const mmdd = today.slice(5).replace('-', '');
+    const serialStr = String(next_serial).padStart(4, '0');
+    const invoiceNumber = `AST-${yy}${mmdd}${serialStr}`;
+
     try {
       await this._prisma.invoice.create({
         data: {
-          invoiceNumber: new Date().toISOString(),
-          invoiceDate: new Date(),
-          deliveryDate: new Date(createInvoiceDto.deliveryDate),
+          invoiceNumber,
+          invoiceDate: new Date().toISOString(),
+          deliveryDate: new Date(createInvoiceDto.deliveryDate).toISOString(),
           customerId: createInvoiceDto.customerId,
           invoiceItems: {
             create: createInvoiceDto.items.map((item) => ({
@@ -45,109 +56,65 @@ export class InvoicesService {
     page: number = 1,
     limit: number = 10,
     status = true,
-    queryString: string,
+    queryString?: string,
   ) {
     try {
       const offset = (page - 1) * limit;
+
+      const whereClause: Prisma.InvoiceWhereInput = {
+        isActive: status,
+        ...(queryString && {
+          OR: [
+            {
+              invoiceNumber: {
+                contains: queryString,
+              },
+            },
+            ...(isValid(parseISO(queryString))
+              ? [
+                  {
+                    deliveryDate: {
+                      equals: parseISO(queryString),
+                    },
+                  },
+                  {
+                    invoiceDate: {
+                      equals: parseISO(queryString),
+                    },
+                  },
+                ]
+              : []),
+            {
+              customer: {
+                mobile: {
+                  contains: queryString,
+                },
+              },
+            },
+          ],
+        }),
+      };
 
       const [data, total] = await Promise.all([
         this._prisma.invoice.findMany({
           skip: offset,
           take: limit,
-          orderBy: {
-            id: 'desc',
-          },
-          where: {
-            isActive: status,
-            ...(queryString && {
-              OR: [
-                {
-                  invoiceNumber: {
-                    contains: queryString,
-                  },
-                },
-                ...(isValid(parseISO(queryString))
-                  ? [
-                      {
-                        deliveryDate: {
-                          equals: new Date(queryString),
-                        },
-                      },
-                      {
-                        invoiceDate: {
-                          equals: new Date(queryString),
-                        },
-                      },
-                    ]
-                  : []),
-                {
-                  customer: {
-                    mobile: {
-                      contains: queryString,
-                    },
-                  },
-                },
-              ],
-            }),
-          },
+          orderBy: { id: 'desc' },
+          where: whereClause,
           include: {
             customer: true,
+            invoiceItems: true,
           },
         }),
         this._prisma.invoice.count({
-          where: {
-            isActive: status,
-            ...(queryString && {
-              OR: [
-                {
-                  invoiceNumber: {
-                    contains: queryString,
-                  },
-                },
-                ...(isValid(parseISO(queryString))
-                  ? [
-                      {
-                        deliveryDate: {
-                          equals: new Date(queryString),
-                        },
-                      },
-                      {
-                        invoiceDate: {
-                          equals: new Date(queryString),
-                        },
-                      },
-                    ]
-                  : []),
-                {
-                  customer: {
-                    mobile: {
-                      contains: queryString,
-                    },
-                  },
-                },
-              ],
-            }),
-          },
+          where: whereClause,
         }),
       ]);
 
       const totalPages = Math.ceil(total / limit);
 
-      const invoicesWithItems = await Promise.all(
-        data.map(async (invoice) => {
-          const invoiceItems = await this._prisma.invoiceItem.findMany({
-            where: {},
-          });
-
-          return {
-            ...invoice,
-            items: invoiceItems,
-          };
-        }),
-      );
-
       return this.httpResponseService.generate(HttpStatus.OK, {
-        data: invoicesWithItems,
+        data,
         meta: {
           total,
           page,
