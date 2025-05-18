0fb2e3e58b8d82ed9b5320f413444c77284c4f99
hasanshahrear <hasanshahrear143@gmail.com>
2025-05-18 19:43:56 +0600

    Invoice update method added

--- src/invoices/dto/update-invoice.dto.ts
+++ src/invoices/dto/update-invoice.dto.ts
@@ -1,8 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsBoolean } from 'class-validator';
+import { IsBoolean, IsNumber } from 'class-validator';
 import { CreateInvoiceDto } from './create-invoice.dto';
 
-export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {}
+export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {
+  @IsNumber()
+  id?: number;
+}
 
 export class PatchInvoiceDto extends PartialType(CreateInvoiceDto) {
   @IsBoolean()
